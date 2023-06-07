ef9d7aeb3cc303d400c5a87e7ce678230e7a1443
zulfuqarbaratzade <zulfuqar.baratzade@gmail.com>
2023-06-07 15:57:53 +0400

    for close or discard button

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ ACCOUNT_PASSWORD = "YOUR LOGIN PASSWORD"
 PHONE = "YOUR PHONE NUMBER"
 
 
+
+def abort_application():
+    # Click Close Button
+    close_button = driver.find_element(by=By.CLASS_NAME, value="artdeco-modal__dismiss")
+    close_button.click()
+
+    time.sleep(2)
+    # Click Discard Button
+    discard_button = driver.find_elements(by=By.CLASS_NAME, value="artdeco-modal__confirm-dialog-btn")[1]
+    discard_button.click()
+
+
+chrome_driver_path = "YOUR CHROME DRIVER PATH"
